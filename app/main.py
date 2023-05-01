416d57e7b63766a08644876fd80f1eaf12e0802a
Jose Manuel <sanchezmosquerajosemanuel@gmail.com>
2023-05-01 12:33:19 +0200

    Add elapsed time to the main program

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@
 #24/08/2022
 
 import os
+import time
 from stockAlerter import StockAlerter
 
 def main():
+
+    startTime = time.time()
+
     rootDir = os.path.dirname(os.path.realpath(__file__)).replace('\\app','').replace('/app','')
     dataFilePath = (rootDir + "\data\stocksData.json").replace('\\', '/')
 
@@ -12,6 +16,10 @@ def main():
     result = stockAlerter.getStockEstimationsTenYears()
     report = stockAlerter.buildReportHTML()
     stockAlerter.sendAlert(report)
+
+    endTime = time.time()
+    elapsedTime = round(endTime - startTime, 2)
+    print('Elapsed time: ', elapsedTime, ' seconds') 
 
 if __name__ == "__main__":
     main()
